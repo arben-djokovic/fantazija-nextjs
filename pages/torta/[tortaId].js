f12b81ad4b00ea2d475d4b9ab776829a1ad291e1
beniiiiii <56236600+beniiiiii@users.noreply.github.com>
2022-04-26 19:03:13 +0200

    fallback set to blocking

--- pages/torta/[tortaId].js
+++ pages/torta/[tortaId].js
@@ -68,7 +68,7 @@ export async function getStaticPaths() {
   
     // We'll pre-render only these paths at build time.
     // { fallback: false } means other routes should 404.
-    return { paths, fallback: false }
+    return { paths, fallback: 'blocking' }
   }
   export async function getStaticProps(constext) {
     // params contains the post `id`.
